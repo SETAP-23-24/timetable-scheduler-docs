a4f62d351aed51daddf03a737bf979656d42b5a6
JakeIV <114679283+JakeIV@users.noreply.github.com>
2024-04-26 12:27:15 +0100

    Update project information in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Timetable Scheduler'
+copyright = '2024'
+author = 'SETAP 23/24 Group 2B'
 
 release = '0.1'
 version = '0.1.0'
